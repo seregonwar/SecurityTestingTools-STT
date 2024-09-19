35779ecab9756f14bb4f5ae24acf3bb22ea8b35e
Seregon <109359355+seregonwar@users.noreply.github.com>
2024-09-19 19:49:18 +0200

    Create main.cpp

---
+++ 900/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "data_processing.h" // Make sure the file exists in the include directory
+#include "syscall_handler.h"
+#include "msr_reader.h"
+#include "memory_accessor.h"
+#include "memory_function.h"
+#include "interrupt_handler.h"
+#include "memory_checker.h"
+#include "memory_initializer.h"
+#include "data_validator.h"
+#include "cr0_handler.h"
+#include "data_processor.h"
+#include "bit_extractor.h"
+#include "data_handler.h"
+#include "memory_caller.h"
+#include "data_encoder.h"
+#include "data_decoder.h"
+#include "data_cleaner.h"
+#include "checksum_calculator.h"
+#include "syscall_caller.h"
+
+int main() {
+    // the main is empty, no specific operation to perform
+    return 0;
+}
