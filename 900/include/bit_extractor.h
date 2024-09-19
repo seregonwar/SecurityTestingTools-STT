b327b007042f5e4e74ac6e976de44c5ffc39485b
Seregon <109359355+seregonwar@users.noreply.github.com>
2024-09-19 19:51:20 +0200

    Create bit_extractor.h

---
+++ 900/include/bit_extractor.h
@@ -0,0 +1,8 @@
+#ifndef BIT_EXTRACTOR_H
+#define BIT_EXTRACTOR_H
+
+#include <cstdint>
+
+int64_t extractBits(int *param1, int param2, uint8_t **param3, uint64_t param4);
+
+#endif // BIT_EXTRACTOR_H
